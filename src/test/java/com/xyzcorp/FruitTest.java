181deec6938650b7d1a95ef21e24c1a2a09f0ccf
AbdessalaRouabha <Abdessalam.rouabha@cgi.com>
2022-02-02 15:04:06 -0500

    TEST FRUITS

--- src/test/java/com/xyzcorp/FruitTest.java
+++ src/test/java/com/xyzcorp/FruitTest.java
@@ -18,7 +18,7 @@ public class FruitTest {
                         .relaxedHTTPSValidation()
                         .accept(ContentType.JSON)
                         .when()
-                        .get("https://staging.tiered-planet.net/mild-temper/legumes")
+                        .get("https://staging.tiered-planet.net/mild-temper/fruits")
                         .then()
                         .assertThat()
                         .body("[0].description", equalTo("Winter fruit"));
