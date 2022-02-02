fe3eecb2172704135433e944c6e62f109b582af4
AbdessalaRouabha <Abdessalam.rouabha@cgi.com>
2022-02-02 11:54:36 -0500

    Adde Jenkins file for API

---
+++ Jenkinsfile
@@ -0,0 +1,20 @@
+pipeline {
+   agent any
+
+   tools {
+      maven 'maven-3.8.4'
+      jdk 'JDK 17'
+   }
+
+   stages {
+      stage('Checkout') {
+         steps{
+            checkout scm
+         }
+      }
+      stage('Build') {
+        steps {
+            sh 'mvn clean verify'
+        }
+      }
+   }
---
+++ src/test/java/com/xyzcorp/FruitTest.java
@@ -0,0 +1,26 @@
+package com.xyzcorp;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+
+public class FruitTest {
+    // Rest-Assured
+    //[{"description":"Winter fruit","name":"Apple"} => [0]
+    // {"description":"Tropical fruit","name":"Pineapple"}] => [1]
+    @Test
+                public  void testSomeThing () {
+
+                given()
+                        .relaxedHTTPSValidation()
+                        .accept(ContentType.JSON)
+                        .when()
+                        .get("https://staging.tiered-planet.net/mild-temper/fruits\n")
+                        .then()
+                        .assertThat()
+                        .body("[0].description", equalTo("Winter fruit"));
+    }
+}
